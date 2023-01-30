923bf3a2305fbaae88d769c05a2a400159ea7211
JackJayJames <ciklops@seznam.cz>
2023-01-30 17:54:54 +0100

    schování do privatni

--- scripty/moduly/Sprava_pojisteni.js
+++ scripty/moduly/Sprava_pojisteni.js
@@ -1,28 +1,42 @@
 'use strict';
 import { Pojistenec } from "./Pojistenec.js";
+import { Validace } from "./Validace.js";
 
 const privatni = new WeakMap();
 export class Sprava_pojisteni {
     constructor(){
         privatni.set(this, {
             _seznam_pojistencu: [],
-        });
+            validace: new Validace(),
 
+            _vypis_pojistencu: document.querySelector("#cont_pojistenci"),
+            _form_jmeno: document.querySelector("#input-jmeno"),
+            _form_prijmeni: document.querySelector("#input-prijmeni"),
+            _form_vek: document.querySelector("#input-vek"),
+            _form_phone: document.querySelector("#input-telefon"),
+            _form_btn: document.querySelector("#btn-ulozit"),
+
+            _spustit: function(){
+                this._form_btn.onclick = () => {
+                    if(!this._zvalidovat())    console.log("Špatný input");
+                };
+            },
+            _zvalidovat: function(){
+                console.log(this);
+                return false;
+            },
+        });
+        this.spustit();
+
+        /*
         this.vypis_pojistencu = document.querySelector("#cont_pojistenci");
         this.form_jmeno = document.querySelector("#input-jmeno");
         this.form_prijmeni = document.querySelector("#input-prijmeni");
         this.form_vek = document.querySelector("#input-vek");
         this.form_phone = document.querySelector("#input-telefon");
-        this.form_btn = document.querySelector("#btn-ulozit");
-
-        this._sputit();
+        this.form_btn = document.querySelector("#btn-ulozit");*/
     }
-    _sputit(){
-        this.form_btn.onclick = () => {
-            if(!this._zvalidovat)    console.log("Správný input");
-        };
-    }
-    _zvalidovat(){
-        
+    spustit(){
+        privatni.get(this)._spustit();
     }
 }
