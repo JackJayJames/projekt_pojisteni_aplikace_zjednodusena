3e6f0adf35dcc9776a49f90a0364c4dbe0a0cacc
JackJayJames <ciklops@seznam.cz>
2023-01-30 17:13:03 +0100

    přidány selectory
    selectory na elementy v html

--- scripty/moduly/Sprava_pojisteni.js
+++ scripty/moduly/Sprava_pojisteni.js
@@ -3,6 +3,11 @@
 const privatni = new WeakMap();
 export class Sprava_pojisteni {
     constructor(){
-        
+        this.vypis_pojistencu = document.querySelector("#cont_pojistenci");
+        this.form_jmeno = document.querySelector("#input-jmeno");
+        this.form_prijmeni = document.querySelector("#input-prijmeni");
+        this.form_vek = document.querySelector("#input-vek");
+        this.form_phone = document.querySelector("#input-telefon");
+        this.form_btn = document.querySelector("#btn-ulozit");
     }
 }
