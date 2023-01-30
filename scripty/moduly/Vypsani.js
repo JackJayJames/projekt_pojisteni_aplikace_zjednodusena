2c1dea3d23080044ae2568ddd48e142624401095
JackJayJames <ciklops@seznam.cz>
2023-01-30 21:13:41 +0100

    vykreslení tabulky

--- scripty/moduly/Vypsani.js
+++ scripty/moduly/Vypsani.js
@@ -10,6 +10,12 @@ export class Vypsani{
 
         const tabulka = document.createElement("table");
         tabulka.appendChild(this._ziskatHlavicku());
+        
+        const tBody = document.createElement("tbody");
+        tabulka.appendChild(tBody);
+        for(const pojistenec of data){
+            tBody.appendChild(this._ziskatRadek(pojistenec));
+        }
 
         element.appendChild(tabulka);
     }
@@ -18,12 +24,20 @@ export class Vypsani{
         const radek = document.createElement("tr");
         hlavicka.appendChild(radek);
 
-        radek.appendChild(this._ziskatBunku("th", "Jmeno"));
-        radek.appendChild(this._ziskatBunku("th", "Příjmení"));
+        radek.appendChild(this._ziskatBunku("th", "Jméno a příjmení"));
         radek.appendChild(this._ziskatBunku("th", "Věk"));
         radek.appendChild(this._ziskatBunku("th", "Telefon"));
 
         return hlavicka;
+    }
+    _ziskatRadek(pojistenec){
+        const radek = document.createElement("tr");
+        
+        radek.appendChild(this._ziskatBunku("td", `${pojistenec["jmeno"]} ${pojistenec["prijmeni"]}`));
+        radek.appendChild(this._ziskatBunku("td", pojistenec["vek"]));
+        radek.appendChild(this._ziskatBunku("td", pojistenec["telefon"]));
+
+        return radek;
     }
     _ziskatBunku(elem, text){
         const bunka = document.createElement(elem);
