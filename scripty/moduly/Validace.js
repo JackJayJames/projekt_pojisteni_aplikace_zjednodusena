30a7a2c556746d23d9ff8ccfd5a8eff2486a5a40
JackJayJames <ciklops@seznam.cz>
2023-01-30 19:03:25 +0100

    metoda pro kontrolu že vstup neni víc slov

--- scripty/moduly/Validace.js
+++ scripty/moduly/Validace.js
@@ -5,6 +5,7 @@ export class Validace{
         if(this._neniPrazdne(text))  return "Toto pole je poviné";
         if(this._nemaSpravnouDelku(text, 2, 20)) return "Vstup musí bát delší 1 písmeno a kratší 21 písmen";
         if(this._obsahujeCisla(text)) return "Vstup nesmí obsahovat čísla";
+        if(this._jeJenomJednoSlovo(text)) return "Vstup musí být jedno slovo";
     }
     prijmeni(){
         
@@ -30,5 +31,12 @@ export class Validace{
     }
     _obsahujeCisla(str){
         return /[0-9]/.test(str);
+    }
+    _jeJenomJednoSlovo(str){
+        const slova = str.split(" ");
+        if(slova.length > 1)
+            return true;
+        else
+            return false;
     }
 }
